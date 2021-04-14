655c622af97a394e4287e80d5c2b62f8bdda0d5e
Paz Marzana <pazmarzana@gmail.com>
2021-04-14 14:51:40 -0300

    Added some styles imports in index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/style.default.css';
+import './css/custom.css';
+import './css/fontastic.css';
 
 ReactDOM.render(
   <React.StrictMode>

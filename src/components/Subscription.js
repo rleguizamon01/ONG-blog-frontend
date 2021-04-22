9ca09d9b8b68e0017037590ab979ab28a5de1f39
RLeguizamon <leguizamon.rodolfo@gmail.com>
2021-04-22 01:29:57 -0300

    Subscription comp. ingrated with react-bootstrap

--- src/components/Subscription.js
+++ src/components/Subscription.js
@@ -1,54 +1,55 @@
 import React from 'react'
+import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 
 export const Subscription = () => {
     return (
         <section className="newsletter no-padding-top m-3">
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-6">
+            <Container>
+                <Row>
+                    <Col md={6}>
                         <h2>Suscribite a nuestra Newsletter</h2>
                         <p className="text-big">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                    </div>
-                    <div className="col-md-12">
+                    </Col>
+                    <Col md={12}>
                         <div className="form-holder">
-                            <form method="post" action="{{route('subscribers.store')}}">
+                            <Form method="post" action="{{route('subscribers.store')}}">
                                 {/* @csrf   CAMBIAR */}
-                                <div className="form-row align-items-end">
-                                    <div className="col-12 col-md-3">
-                                        <label htmlFor="first_name">Nombre</label>
-                                        <input type="text" className="form-control form-control-sm" id="first_name" name="first_name" placeholder="Ingrese su nombre ..." defaultValue=""/>
+                                <Form.Row className="align-items-end">
+                                    <Col xs={12} md={3}>
+                                        <Form.Label htmlFor="first_name">Nombre</Form.Label>
+                                        <Form.Control type="text" size="sm" id="first_name" name="first_name" placeholder="Ingrese su nombre ..." defaultValue=""/>
 
                                         {/* @error('first_name')    CAMBIAR */}
                                             {/* <div className="error"> {{ $errors->first('first_name') }} </div>  CAMBIAR */}
                                         {/* @enderror */}
-                                    </div>
+                                    </Col>
 
-                                    <div className="col-12 col-md-3">
-                                        <label htmlFor="last_name">Apellido</label>
-                                        <input type="text" className="form-control form-control-sm" id="last_name" name="last_name" placeholder="Ingrese su apellido" defaultValue=""/>
+                                    <Col xs={12} md={3}>
+                                        <Form.Label htmlFor="last_name">Apellido</Form.Label>
+                                        <Form.Control type="text" size="sm" id="last_name" name="last_name" placeholder="Ingrese su apellido" defaultValue=""/>
 
                                         {/* @error('last_name')   CAMBIAR */}
                                             {/* <div className="error"> {{ $errors->first('last_name') }} </div>   CAMBIAR */}
                                         {/* @enderror    CAMBIAR */}
-                                    </div>
+                                    </Col>
 
-                                    <div className="col-12 col-md-3">
-                                        <label htmlFor="email">Email</label>
-                                        <input type="email" className="form-control form-control-sm" id="email" name="email" placeholder="Ingrese su email" defaultValue=""/>
+                                    <Col xs={12} md={3}>
+                                        <Form.Label htmlFor="email">Email</Form.Label>
+                                        <Form.Control type="email" size="sm" id="email" name="email" placeholder="Ingrese su email" defaultValue=""/>
 
                                         {/* @error('email')    CAMBIAR */}
                                             {/* <div className="error"> {{ $errors->first('email') }} </div>   CAMBIAR */}
                                         {/* @enderror    CAMBIAR */}
-                                    </div>
-                                    <div className="col-12 col-md-3">
-                                        <button type="submit" className="submit mt-4">Suscribirse</button>
-                                    </div>
-                                </div>
-                            </form>
+                                    </Col>
+                                    <Col xs={12} md={3}>
+                                        <Button type="submit" className="submit mt-4">Suscribirse</Button>
+                                    </Col>
+                                </Form.Row>
+                            </Form>
                         </div>
-                    </div>
-                </div>
-            </div>
+                    </Col>
+                </Row>
+            </Container>
         </section>
     )
 }

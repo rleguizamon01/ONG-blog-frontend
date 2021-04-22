d6b1b03f2c5b1d92a1e6f3b8f32effddcf3fa14a
RLeguizamon <leguizamon.rodolfo@gmail.com>
2021-04-22 01:30:03 -0300

    Posts comp. ingrated with react-bootstrap

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,33 +1,36 @@
 import React from 'react';
+import { Container, Card, Row } from 'react-bootstrap';
 
 export const Posts = () => {
     return(
-        <div className="container">
-            <div className="row">
-                <div className="card col-xl-6 border-0 mb-5 pr-5 pl-5">
-                    <div className="card-body">
+        <Container>
+            <Row>
+                <Card border="0" className="col-xl-6 mb-5 pr-5 pl-5">
+                    <Card.Body>
                          {/*Photo*/}
-                        <a href="#"><img src="https://images.unsplash.com/photo-1593642532400-2682810df593?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80" className="card-img-top mb-3"></img></a>
+                         <Card.Img variant="top" src="https://images.unsplash.com/photo-1593642532400-2682810df593?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80" className="mb-3"></Card.Img>
                          {/*Title*/}
                         <a href="{{ route('posts.show', $post->id) }}" className="text-decoration-none">
-                            <h5 className="card-title">Titulo</h5>
+                            <Card.Title>Titulo</Card.Title>
                         </a>
-                        <div className="d-flex justify-content-between mt-2 mb-2">
-                            {/* Date*/}
-                            <div className="text-uppercase text-muted font-monospace">
-                                Fecha de creacion
+                        <Card.Text>
+                            <div className="d-flex justify-content-between mt-2 mb-2">
+                                {/* Date*/}
+                                <div className="text-uppercase text-muted font-monospace">
+                                    Fecha de creacion
+                                </div>
+                                {/*Category*/}
+                                <div className="text-capitalize text-muted">
+                                    Categoria: categoria
+                                </div>
                             </div>
-                             {/*Category*/}
-                            <div className="text-capitalize text-muted">
-                                Categoria: categoria
-                            </div>
-                        </div>
-                         {/*Author*/}
-                        <p className="card-text text-muted fw-bold">Nombre autor</p>
-                        </div>
-                </div>
-            </div>
+                            {/*Author*/}
+                            <p className="card-text text-muted fw-bold">Nombre autor</p>
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </Row>
             <div className="d-flex justify-content-center mb-4">links</div>
-        </div>
+        </Container>
     );
 };

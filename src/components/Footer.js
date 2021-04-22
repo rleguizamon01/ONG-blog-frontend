9a1df6a831b4c254c5ca8ceb816ffc180e4720f2
RLeguizamon <leguizamon.rodolfo@gmail.com>
2021-04-22 01:31:05 -0300

    Footer component integrated with react-bootstrap

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
 
 export const Footer = () => {
     return (
         <footer className="main-footer">
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-8">
+            <Container>
+                <Row>
+                    <Col md={8}>
                         <div className="logo">
                             <h6 className="text-white">ONG TEAM</h6>
                         </div>
@@ -21,8 +22,8 @@ export const Footer = () => {
                                 <li className="list-inline-item"><a href="#"><i className="fab fa-pinterest"></i></a></li>
                             </ul>
                         </div>
-                    </div>
-                    <div className="col-md-4">
+                    </Col>
+                    <Col md={4}>
                         <div className="menus d-flex">
                             <ul className="list-unstyled">
                                 <li><a href="#">Blog</a></li>
@@ -30,22 +31,22 @@ export const Footer = () => {
                                 <li><a href="#">Subscribers</a></li>
                             </ul>
                         </div>
-                    </div>
-                </div>
-            </div>
+                    </Col>
+                </Row>
+            </Container>
             <div className="copyrights">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-6">
+                <Container>
+                    <Row>
+                        <Col md={6}>
                             <p>&copy; 2017. All rights reserved. Your great site.</p>
-                        </div>
-                        <div className="col-md-6 text-right">
+                        </Col>
+                        <Col md={6} className="text-right">
                             <p>Template By <a href="https://bootstrapious.com/p/bootstrap-carousel" className="text-white">Bootstrapious</a>
                                 {/* <!-- Please do not remove the backlink to Bootstrap Temple unless you purchase an attribution-free license @ Bootstrap Temple or support us at http://bootstrapious.com/donate. It is part of the license conditions. Thanks for understanding :)                         --> */}
                             </p>
-                        </div>
-                    </div>
-                </div>
+                        </Col>
+                    </Row>
+                </Container>
             </div>
         </footer>
     )

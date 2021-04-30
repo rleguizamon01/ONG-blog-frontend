15b5200b77b3892fea8291b97eb26e8ed11630d1
RLeguizamon <leguizamon.rodolfo@gmail.com>
2021-04-30 20:59:34 -0300

    Routes to category added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
 import Home from './components/Home';
-import { Posts } from './components/Posts';
+//import { Posts } from './components/Posts';
 import Donation from './components/Donation';
 import Subscription  from './components/Subscription';
 import Volunteers from './components/Volunteers';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Post from "./components/Post";
 import {Container} from "react-bootstrap";
+import Category from './components/Category';
+import CategoryAdd from './components/CategoryAdd';
+import CategoryEdit from './components/CategoryEdit';
 
 function App() {
   return (
@@ -21,9 +24,9 @@ function App() {
           <Route path='/posts/:id'>
             <Post/>
           </Route>
-          <Route path='/posts'>
+          {/*<Route path='/posts'>
             <Posts/>
-          </Route>
+          </Route>*/}
           <Route path='/volunteers/create'>
             <Volunteers/>
           </Route>
@@ -33,6 +36,17 @@ function App() {
           <Route path='/subscribers/create'>
             <Subscription/>
           </Route>
+          <Route path='/categories/add'>
+            <CategoryAdd/>
+          </Route>
+          <Route path='/categories/edit/:id'>
+            <CategoryEdit/>
+          </Route>
+          <Route path='/categories'>
+            <Category/>
+          </Route>
+          
+          
         </Switch>
         <Footer/>
       </Container>

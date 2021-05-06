5c356da23e92aeabe8c90a1d117a2365d63cc1bb
Nahuel Savedra <nsavedra97@gmail.com>
2021-05-06 16:19:36 -0300

    Add yup and formik in login and register

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,19 @@
-import React, {useState,useEffect} from 'react';
+import React, {useEffect} from 'react';
 import { Button } from 'react-bootstrap';
 import { useHistory } from "react-router-dom";
 import Header from './Header'
+import axios from "axios";
+import {Formik, Form, Field, ErrorMessage} from 'formik';
+import * as Yup from "yup";
 
 const Login = () => {
     let history = useHistory();
-    const [email,SetEmail]=useState("");
-    const [password,SePassword]=useState("");
+    const formSchema = Yup.object().shape({
+        email: Yup.string().email('El email debe tener el formato "mi_email@ejemplo.com"')
+            .required('Debe ingresar dirección de email.'),
+        password: Yup.string('Debe ingresar una contraseña valida.')
+            .required('Debe ingresar una contraseña.'),
+    });
 
     useEffect(() => {
         if(localStorage.getItem('user-info'))
@@ -15,30 +22,69 @@ const Login = () => {
         }
     }, [])
 
-    async function login(){
-        const item={email,password};
-        let result = await fetch("http://127.0.0.1:8000/api/login",{
-            method:'POST',
-            body:JSON.stringify(item),
-            headers:{
-                "Content-Type":"application/json",
-                "Accept":"application/json"
-            }
-        });
-        result = await result.json();
-        localStorage.setItem("user-info",JSON.stringify(result));
-        history.push("/");
-    }
-
     return (
         <>
         <Header/>
-            <div className="col-sm-10 offset-sm-5">
+            <Formik initialValues={{
+                email: "",
+                password: "",
+            }}
+                    validationSchema={formSchema}
+                    onSubmit={(values) =>
+                    {
+                        axios({
+                            method:'POST',
+                            url: "http://127.0.0.1:8000/api/login",
+                            data: {
+                                email: values.email,
+                                password: values.password
+                            },
+                            headers:{
+                                'Access-Control-Allow-Origin' : '*',
+                                'Access-Control-Allow-Methods' : 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+                                "Content-Type":"application/json",
+                                "Accept":"application/json"
+                            }
+                        })
+                            .then(res => localStorage.setItem("user-info",JSON.stringify(res)));
+                        history.push("/");
+                    }}
+            >
+            <div className="col-sm-6 offset-sm-3">
                 <h1>Login</h1>
-                <div className="my-2"><input type="email" value={email} onChange={(event)=>SetEmail(event.target.value)} className="form-controler" placeholder="email"/></div>
-                <div className="my-2"><input type="password" value={password} onChange={(event)=>SePassword(event.target.value)} className="form-controler" placeholder="password"/></div>
-                <Button onClick={login}>Ingresar</Button>
+                <Form>
+                <div className="form-group">
+                        <label htmlFor="email">Email</label>
+                        <Field
+                            className='form-control'
+                            name='email'
+                            placeholder=''
+                            type='text'
+                        />
+                        <ErrorMessage
+                            name='email'
+                            component='div'
+                            className='field-error text-danger'
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="password">Password</label>
+                        <Field
+                            className='form-control'
+                            name='password'
+                            placeholder=''
+                            type='password'
+                        />
+                        <ErrorMessage
+                            name='password'
+                            component='div'
+                            className='field-error text-danger'
+                        />
+                    </div>
+                 <Button type="submit" className="btn btn-success">Ingresar</Button>
+                </Form>
             </div>
+            </Formik>
         </>
     )
 }
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,13 +1,22 @@
-import React, {useState,useEffect} from 'react';
-import { Button } from 'react-bootstrap';
+import React, {useEffect} from 'react';
 import { useHistory } from "react-router-dom";
-import Header from './Header'
+import Header from './Header';
+import * as Yup from 'yup';
+import {Formik, Form, Field, ErrorMessage} from 'formik';
+import axios from 'axios';
+
 
 const Register = () => {
-    const [first_name,SetFirst_name]=useState("");
-    const [last_name,SetLast_name]=useState("");
-    const [email,SetEmail]=useState("");
-    const [password,SePassword]=useState("");
+    const formSchema = Yup.object().shape({
+        first_name: Yup.string('Debe ingresar un nombre válido.')
+            .required('Debe ingresar un nombre.'),
+        last_name: Yup.string('Debe ingresar un apellido válido.')
+            .required('Debe ingresar un apellido.'),
+        email: Yup.string().email('El email debe tener el formato "mi_email@ejemplo.com"')
+            .required('Debe ingresar dirección de email.'),
+        password: Yup.string('Debe ingresar una contraseña valida.')
+            .required('Debe ingresar una contraseña.'),
+    });
     let history = useHistory();
 
     useEffect(() => {
@@ -17,37 +26,107 @@ const Register = () => {
         }
     }, [])
 
-    async function register(){
-        const item={first_name,last_name,email,password};
-        let result = await fetch("http://127.0.0.1:8000/api/register",{
-            method:'POST',
-            body:JSON.stringify(item),
-            headers:{
-                'Access-Control-Allow-Origin' : '*',
-                'Access-Control-Allow-Methods' : 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
-                "Content-Type":"application/json",
-                "Accept":"application/json"
-            }
-        });
-        result = await result.json();
-        localStorage.setItem("user-info",JSON.stringify(result));
-        history.push("/");
-
-    }
-
     return (
         <>
             <Header/>
-            <div className="col-sm-10 offset-sm-5">
-                <h1>Register</h1>
-                <div className="my-2"><input type="text" value={first_name} onChange={(event)=>SetFirst_name(event.target.value)} className="form-controler" placeholder="first_name"/></div>
-                <div className="my-2"><input type="text" value={last_name} onChange={(event)=>SetLast_name(event.target.value)} className="form-controler" placeholder="last_name"/></div>
-                <div className="my-2"><input type="email" value={email} onChange={(event)=>SetEmail(event.target.value)} className="form-controler" placeholder="email"/></div>
-                <div className="my-2"><input type="password" value={password} onChange={(event)=>SePassword(event.target.value)} className="form-controler" placeholder="password"/></div>
-                <Button onClick={register}>Registrarse</Button>
-            </div>
+            <Formik initialValues={{
+                first_name: "",
+                last_name: "",
+                email: "",
+                password: "",
+            }}
+                    validationSchema={formSchema}
+                    onSubmit={(values) =>
+                    {
+                        axios({
+                            method:'POST',
+                            url: "http://127.0.0.1:8000/api/register",
+                            data: {
+                                first_name: values.first_name,
+                                last_name: values.last_name,
+                                email: values.email,
+                                password: values.password
+                            },
+                            headers:{
+                                'Access-Control-Allow-Origin' : '*',
+                                'Access-Control-Allow-Methods' : 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+                                "Content-Type":"application/json",
+                                "Accept":"application/json"
+                            }
+                        })
+                            .then(res => localStorage.setItem("user-info",JSON.stringify(res)));
+                        history.push("/");
+
+                    }}
+            >
+                <div className="col-sm-6 offset-sm-3">
+                    <h3>Registrarse</h3>
+                    <Form>
+                    <div className="form-group">
+                        <label htmlFor="first_name">Nombre</label>
+                        <Field
+                            className='form-control'
+                            name='first_name'
+                            placeholder=''
+                            type='text'
+                        />
+                        <ErrorMessage
+                            name='first_name'
+                            component='div'
+                            className='field-error text-danger'
+                        />
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="last_name">Apellido</label>
+                        <Field
+                            className='form-control'
+                            name='last_name'
+                            placeholder=''
+                            type='text'
+                        />
+                        <ErrorMessage
+                            name='last_name'
+                            component='div'
+                            className='field-error text-danger'
+                        />
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="email">Email</label>
+                        <Field
+                            className='form-control'
+                            name='email'
+                            placeholder=''
+                            type='text'
+                        />
+                        <ErrorMessage
+                            name='email'
+                            component='div'
+                            className='field-error text-danger'
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="password">Password</label>
+                        <Field
+                            className='form-control'
+                            name='password'
+                            placeholder=''
+                            type='password'
+                        />
+                        <ErrorMessage
+                            name='password'
+                            component='div'
+                            className='field-error text-danger'
+                        />
+                    </div>
+
+                    <button type="submit" className="btn btn-primary mt-2">Registrarse</button>
+                    </Form>
+                </div>
+            </Formik>
         </>
-    )
+    );
 }
 
-export default Register
+export default Register;
